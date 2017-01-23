08c6deb629f9c1ef52bf6c89ec349b6497193f26
Jarrod2879 <jarrod@grumpysailor.com>
2017-01-23 23:16:54 +1100

    Added Site Code

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function() {
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+autoplaySpeed: 2500,
+});
+});
